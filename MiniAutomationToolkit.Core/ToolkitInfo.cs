b4658be740289f6a29cb77eddb61bf5fd2bfbfdb
Jorjetta <jorjettamkryan@gmail.com>
2026-08-06 19:16:57 +0400

    Блок 1. Структура решения и Git

--- MiniAutomationToolkit.Core/ToolkitInfo.cs
+++ MiniAutomationToolkit.Core/ToolkitInfo.cs
@@ -1,13 +1,9 @@
 namespace MiniAutomationToolkit.Core;
 
-/// <summary>
-/// Общая информация о наборе инструментов, переиспользуемая клиентскими приложениями.
-/// </summary>
+
 public static class ToolkitInfo
 {
-    /// <summary>Отображаемое имя набора инструментов.</summary>
     public const string Name = "MiniAutomationToolkit";
-
-    /// <summary>Сообщение, которое приложение выводит при запуске.</summary>
+    
     public static string StartupMessage => $"{Name} started";
 }
